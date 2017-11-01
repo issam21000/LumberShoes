6f583dc2598e6afa6641a6ce521aa52f041527c7
Issam_MEDJMADJ <issam21000@live.fr>
2017-11-01 21:32:18 +0100

    add search by legend in ShoesController

--- app/src/Controllers/ShoesController.php
+++ app/src/Controllers/ShoesController.php
@@ -52,10 +52,10 @@ final class ShoesController extends BaseController
     }
 
     public function searchShoes(Request $request, Response $response, $args){
-       
+        
         $search=$_GET['query'];
         if(null !== $_GET && $_GET['search']=='submit'){
-            $shoes=Shoes::where('model', 'like', "%$search%")->orWhere('description', 'like', "%$search%")->distinct()->get(); 
+            $shoes=Shoes::where('model', 'like', "%$search%")->orWhere('description', 'like', "%$search%")->orWhereIn('brand_id', Brand::where('legend','like',"%$search%")->distinct()->get())->distinct()->get();; 
         } 
         return $this->container->view->render($response, 'shoes.twig',['shoes' => $shoes]);          
     }
